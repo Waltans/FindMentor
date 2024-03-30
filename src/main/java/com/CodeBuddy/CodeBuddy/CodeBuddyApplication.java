82f72d758e4d171fca92c1a29def227f74053e89
Waltan <suntsovvl00@yandex.ru>
2024-03-30 17:35:14 +0500

    Spring Initializr

---
+++ src/main/java/com/CodeBuddy/CodeBuddy/CodeBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.CodeBuddy.CodeBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodeBuddy/CodeBuddy/CodeBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeBuddy.CodeBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
