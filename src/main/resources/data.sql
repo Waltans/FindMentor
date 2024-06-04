9eba85b3bfc51d4a19bab1323be783c1fe138783
Alexey <alexshamox@gmail.com>
2024-06-05 00:31:53 +0500

    Добавлене файла data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+insert into keyword (keyword) values ('Java');
+insert into keyword (keyword) values ('C#');
+insert into keyword (keyword) values ('C++');
+insert into keyword (keyword) values ('опыт 5 лет');
+insert into keyword (keyword) values ('JavaScript');
+insert into keyword (keyword) values ('PHP');
+insert into keyword (keyword) values ('о');
+insert into keyword (keyword) values ('опыт 5 лет');
+insert into keyword (keyword) values ('Java');
+insert into keyword (keyword) values ('C#');
+insert into keyword (keyword) values ('C++');
+insert into keyword (keyword) values ('опыт 10 лет');
+insert into keyword (keyword) values ('Python');
+insert into keyword (keyword) values ('TypeScript');
+insert into keyword (keyword) values ('SQL');
+insert into keyword (keyword) values ('Swift');
+insert into keyword (keyword) values ('Go');
+insert into keyword (keyword) values ('Rust');
+insert into keyword (keyword) values ('Lua');
+insert into keyword (keyword) values ('HTML/CSS');
+
+
